22fe238ded3c5e69e1dfce51816ac9ba23510037
Tomaz505 <tomaz.plavcak505@gmail.com>
2026-01-09 21:36:53 +0100

    popravki

--- modul.f90
+++ modul.f90
@@ -12,31 +12,15 @@ module rutine
         !xy - koordinate vozlisc
         integer :: n,deg
         real(dp) :: a, xy(:,:)
-        if      (deg == 0) then
-            do i =1,n-1
-            a = a+(xy(1,i)*xy(2,i+1)-xy(1,i+1)*xy(2,i))
+
+        do i=1,n-1
+            x = 0.0
+            do j= 0,deg
+                x = x + xy(2,i)**(deg-j)*xy(2,i+1)**(j)
             end do
-        else if (deg == 1) then
-            do i =1,n-1
-            a = a+(xy(1,i)*xy(2,i+1)-xy(1,i+1)*xy(2,i))*(xy(2,i)+xy(2,i+1))/3.0_dp
-            end do
-        else if (deg == 2) then
-            do i =1,n-1
-            a = a+(xy(1,i)*xy(2,i+1)-xy(1,i+1)*xy(2,i))*((xy(2,i)+xy(2,i+1))**2-xy(2,i)*xy(2,i+1))/6.0_dp
-            end do
-        else if (deg == 3) then
-            do i =1,n-1
-            a = a+(xy(1,i)*xy(2,i+1)-xy(1,i+1)*xy(2,i))*(xy(2,i)+xy(2,i+1))*(xy(2,i)**2+xy(2,i+1**2))/10.0_dp
-            end do
-        else if (deg == 4) then
-            do i =1,n-1
-                a = a+(xy(1,i)*xy(2,i+1) - xy(1,i+1)*xy(2,i))*((xy(2,i)**2+xy(2,i)*xy(2,i+1)+xy(2,i+1)**2)**2-xy(2,i)*xy(2,i+1)*(xy(2,i)+xy(2,i+1))**2)/15.0_dp
-            end do
-        else if (deg == 5) then
-            do i =1,n-1
-                a = a+(xy(1,i)*xy(2,i+1) - xy(1,i+1)*xy(2,i))*(xy(2,i)+xy(2,i+1))*(xy(2,i)**2+xy(2,i+1)*xy(2,i)+xy(2,i+1)**2)*(xy(2,i)**2-xy(2,i+1)*xy(2,i)+xy(2,i+1)**2)/21.0_dp
-            end do
-        end if
+            a = a + x*(xy(1,i)*xy(2,i+1)-xy(1,i+1)*xy(2,i))*2/(1+deg)/(2+deg)
+        end do
+
         return
     end subroutine
 
